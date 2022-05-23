02fffd3f80667027e83e0e0a124062fdaa40f24c
Washh <washington.igor22@gmail.com>
2022-05-23 20:43:22 -0300

    Aula 2

--- node/NLW-Valoriza/src/server.ts
+++ node/NLW-Valoriza/src/server.ts
@@ -1,24 +1,14 @@
-import express, { response } from 'express';
+import 'reflect-metadata';
+import express from 'express';
+
+import { router } from './router';
+
+import './database';
 
 const app = express();
 
-/**
- * GET => Buscar uma informação
- * POST => Inserir(Criar) uma informação
- * PUT => Alterar uma informação
- * DELETE => Remover um dado
- * PATCH => Alterar uma informação específica
- */
+app.use(express.json());
 
-app.get('/test', (request, response) => {
-  // Request => Entrando
-  // Response => Saindo
-  return response.send('Enviando resposta, método get');
-});
+app.use(router);
 
-app.post("/test-post", (request, response) => {
-  return response.send('Enviando resposta, método post')
-})
-
-// http://localhost:3000
 app.listen(3000, () => console.log('Server is running'));
